6b710175a82f07b158bd8bcebf5153a1e0196391
Christina  Matson <cmatson93@gmail.com>
2017-12-07 21:28:06 -0800

    working

--- public/app.js
+++ public/app.js
@@ -4,9 +4,6 @@ $(document).on("click", "#noteButton", function(){
   console.log(this.value);
 
   var thisID = this.value;
-  // $('.modal').on('shown.bs.modal', function () {
-  //   $('#myInput').trigger('focus')
-  // })
 
   $.ajax({
     method: "GET",
@@ -19,15 +16,17 @@ $(document).on("click", "#noteButton", function(){
 
     $(".modal").fadeIn(300).css("display", "flex");
     // If there's a note in the article
-    if (data.note) {
+    if (data.note.title) {
       // Place the title of the note in the title input
-      $("#titleinput").val(data.note.title);
-      // Place the body of the note in the body textarea
-      $("#bodyinput").val(data.note.body);
+      $("#note-title").val(data.note.title);
+      
     }
-    
+    if (data.note.body) {
+      // Place the body of the note in the body textarea
+      $("#note-body").val(data.note.body);
+    }
+
     $("#articleTitle").text(data.title);
-// $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
     $(document).on("click", "#savenote", function(){
       console.log(this);
@@ -37,7 +36,7 @@ $(document).on("click", "#noteButton", function(){
         method: "POST",
         url: "/note/"+ newId,
         data: {
-          title: $("note-title").val(),
+          title: $("#note-title").val(),
           body: $("#note-body").val()
         }
       })
@@ -49,35 +48,9 @@ $(document).on("click", "#noteButton", function(){
 
   })
 
+  $(".closeMod").on("click", function(){
+    $(".modal").modal('hide');
+  })
+
 });
 
-
-// When you click the savenote button
-// $(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  // console.log(this);
-  // var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  // $.ajax({
-  //   method: "POST",
-  //   url: "/articles/" + thisId,
-  //   data: {
-  //     // Value taken from title input
-  //     title: $("#titleinput").val(),
-  //     // Value taken from note textarea
-  //     body: $("#bodyinput").val()
-  //   }
-  // })
-  //   // With that done
-  //   .done(function(data) {
-  //     // Log the response
-  //     console.log(data);
-  //     // Empty the notes section
-  //     $("#notes").empty();
-  //   });
-
-  // // Also, remove the values entered in the input and textarea for note entry
-  // $("#titleinput").val("");
-  // $("#bodyinput").val("");
-// });
--- server.js
+++ server.js
@@ -87,16 +87,33 @@ app.get("/scrape", function(req, res){
 //NOTE ROUTES
 //Post a new note
 app.post("/note/:id", function(req, res) {
-  var note = new Note(req.body);
-  note.save(function(err, doc) {
-    if (err) throw err;
-    Article.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, newdoc) {
-      if (err) throw err;
-      else {
-        res.send(newdoc);
-      }
+  // var note = new Note(req.body);
+  // note.save(function(err, doc) {
+  //   if (err) throw err;
+  //   Article.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, newdoc) {
+  //     if (err) throw err;
+  //     else {
+  //       res.send(newdoc);
+  //     }
+  //   });
+  // });
+  console.log(req.body);
+  db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
-  });
 });
 
 
