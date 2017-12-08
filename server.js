1e60e1e32daca3c0442b3a20e6963ebdeaa312de
Christina  Matson <cmatson93@gmail.com>
2017-12-07 22:14:17 -0800

    First deploy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var port = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -172,7 +172,7 @@ app.get("/saved", function(req, res){
 
 
 // Set the app to listen on port 3000
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("App running on port 3000!");
 });
 
