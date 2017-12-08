b1c7fc9ac5172979227f12431ec0da9185eac00d
Christina  Matson <cmatson93@gmail.com>
2017-12-07 23:31:14 -0800

    Cleaned things up

--- public/app.js
+++ public/app.js
@@ -1,8 +1,6 @@
-// Whenever someone clicks a addNote button
+// Whenever someone clicks a noteButton button
 $(document).on("click", "#noteButton", function(){
   event.preventDefault();
-  console.log("addNote button clicked");
-  console.log(this.value);
 
   var thisID = this.value;
 
@@ -10,27 +8,26 @@ $(document).on("click", "#noteButton", function(){
     method: "GET",
     url: "/note/" + thisID
   }).done(function(data){
-    $("#modButtons").prepend("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-    console.log("done");
     console.log(data);
-    console.log("========");
+    $("#modButtons").prepend("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
     $(".modal").fadeIn(300).css("display", "flex");
     // If there's a note in the article
+    console.log(data.note.title);
     if (data.note.title) {
-      // Place the title of the note in the title input
+    //   // Place the title of the note in the title input
       $("#note-title").val(data.note.title);
       
     }
     if (data.note.body) {
-      // Place the body of the note in the body textarea
+    //   // Place the body of the note in the body textarea
       $("#note-body").val(data.note.body);
     }
 
     $("#articleTitle").text(data.title);
 
     $(document).on("click", "#savenote", function(){
-      console.log(this);
+
       var newId = $(this).attr("data-id");
 
       $.ajax({
@@ -42,15 +39,14 @@ $(document).on("click", "#noteButton", function(){
         }
       })
       .done(function(data){
-        console.log(data);
-
+        $(".modal").fadeOut(300).css("display", "none");
       })
     })
 
   })
 
   $(".closeMod").on("click", function(){
-    $(".modal").remove();
+    $(".modal").fadeOut(300).css("display", "none");
   })
 
 });
